d6b93208ee9479380c820848c4bb982db071c397
Joseph Newmann <joseph.newmann@kickup.co>
2025-12-05 09:38:46 -0700

    update diagnostic mapping, add sqlfmt

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,12 +1,13 @@
 return {
   'stevearc/conform.nvim',
   opts = {
-    format_on_save = { timeout_ms = 500, lsp_format = 'fallback' },
+    format_on_save = { async = true, lsp_format = 'fallback' },
     formatters_by_ft = {
       lua = { 'stylua' },
       python = { 'ruff_fix', 'ruff_format' },
       javascript = { 'eslint_d', 'prettier' },
       javascriptreact = { 'eslint_d', 'prettier' },
+      sql = { 'sqlfmt' },
       typescript = { 'eslint_d', 'prettier' },
       typescriptreact = { 'eslint_d', 'prettier' },
     },
