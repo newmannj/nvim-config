8dad57b104d55a6a76e1ba1d841aeb3d4606f5b7
Joseph Newmann <joseph.newmann@kickup.co>
2025-12-12 12:33:53 -0700

    repolace telescope with mini

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -84,7 +84,6 @@ opt.list = true -- Show some invisible characters (tabs...
 opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 opt.mouse = 'a' -- Enable mouse mode
 opt.number = true -- Print line number
-opt.pumblend = 10 -- Popup blend
 opt.pumheight = 10 -- Maximum number of entries in a popup
 opt.relativenumber = true -- Relative line numbers
 opt.ruler = false -- Disable the default ruler
---
+++ lua/plugins/mini-extra.lua
@@ -0,0 +1,17 @@
+return {
+  'nvim-mini/mini.extra',
+  version = '*',
+  dependencies = { 'nvim-mini/mini.pick' },
+  config = function()
+    local pick = require 'mini.pick'
+    -- Find files, live grep, buffers
+    vim.keymap.set('n', '<leader>ff', pick.builtin.files, { desc = '[F]ind [f]iles' })
+    vim.keymap.set('n', '<leader>fg', pick.builtin.grep_live, { desc = '[F]ind with [g]rep' })
+    vim.keymap.set('n', '<leader>fb', pick.builtin.buffers, { desc = '[F]ind in [b]uffers' })
+    local extra = require 'mini.extra'
+    -- vim.keymap.set('n', 'grd', extra.pickers.lsp, { scope = 'definition', desc = '[G]o to [d]efinitions' })
+    vim.keymap.set('n', 'grr', function()
+      extra.pickers.lsp { scope = 'references' }
+    end, { desc = '[G]o to [r]eferences' })
+  end,
+}
